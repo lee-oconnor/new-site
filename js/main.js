55d15f6127da01a11fc56406c71792e2b63af96b
lee-o <oconnor.sherri@gmail.com>
2022-12-11 10:24:46 -0500

    Started making carousel

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const carouselImages = document.querySelector(".img-div");
+
+let slidePosition = 0;
+
+const totalImgs = carouselImages.length;
+
+const slideDotOne = document.querySelector(".btn1");
+const slideDotTwo = document.querySelector(".btn1");
+const slideDotThree = document.querySelector(".btn1");
