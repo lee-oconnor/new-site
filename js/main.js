15d6856a670b2a19754511d24161c35afee4b0f2
lee-o <oconnor.sherri@gmail.com>
2022-12-15 09:02:09 -0500

    Save climb section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const carouselImages = document.querySelector(".img-div");
+// const carouselImages = document.querySelector(".img-div");
 
-let slidePosition = 0;
+// let slidePosition = 0;
 
-const totalImgs = carouselImages.length;
+// const totalImgs = carouselImages.length;
 
-const slideDotOne = document.querySelector(".btn1");
-const slideDotTwo = document.querySelector(".btn1");
-const slideDotThree = document.querySelector(".btn1");
+// const slideDotOne = document.querySelector(".btn1");
+// const slideDotTwo = document.querySelector(".btn1");
+// const slideDotThree = document.querySelector(".btn1");
+
+//To make nav link text change color when scrolling
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 780) {
+    nav.classList.add("active-nav");
+  } else {
+    nav.classList.remove("active-nav");
+  }
+});
