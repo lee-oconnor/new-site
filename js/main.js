189fa82356868d32c80f9d6ebefa13528129bc1e
lee-o <oconnor.sherri@gmail.com>
2022-12-18 14:32:03 -0500

    fixing nxt btn positioning

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-// const carouselImages = document.querySelector(".img-container");
+//Buttoms for making carousel images scroll forward/backwards
+const slides = document.querySelector(".carousel-item");
 
-// let slidePosition = 0;
+let slidePosition = 0;
 
-// const totalImgs = carouselImages.length;
+const totalSlides = slides.length;
 
-// const slideDotOne = document.querySelector(".btn1");
-// const slideDotTwo = document.querySelector(".btn1");
-// const slideDotThree = document.querySelector(".btn1");
+document.querySelector(".nxt-btn").addEventListener("click", function () {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+});
+
+document.querySelector(".pre-btn").addEventListener("click", function () {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+});
 
 //To make nav link text change color when scrolling
 const nav = document.querySelector("nav");
