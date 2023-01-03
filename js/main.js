73085122bc94c4a53cd945819b2d2479d2babe90
lee-o <oconnor.sherri@gmail.com>
2023-01-03 12:46:59 -0500

    Finished styling

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 //Buttoms for making carousel images scroll forward/backwards
 
-const slides = document.querySelector(".img-container");
+const slides = [...document.querySelectorAll(".img-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
 
-// const slides = [...document.querySelectorAll(".img-container")];
-// const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
-// const preBtn = [...document.querySelectorAll(".pre-btn")];
+slides.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
 
-// slides.forEach((item, i) => {
-//   let containerDimensions = item.getBoundingClientRect();
-//   let containerWidth = containerDimensions.width;
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
 
-//   nxtBtn[i].addEventListener("click", () => {
-//     item.scrollLeft += containerWidth;
-//   });
-
-//   preBtn[i].addEventListener("click", () => {
-//     item.scrollLeft -= containerWidth;
-//   });
-// });
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
 
 //To make nav link text change color when scrolling
 const nav = document.querySelector("nav");
